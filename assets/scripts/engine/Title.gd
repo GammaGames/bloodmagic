af8e62cd6020ed059280980fc374b8c4b8805d97
GammaGames <jesse007@ymail.com>
2018-08-13 23:06:58 -0600

    add main menu

---
+++ assets/scripts/engine/Title.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	pause()
+	$Boxes/Center/Buttons/Play.connect("pressed", self, "play")
+	$Boxes/Center/Buttons/Quit.connect("pressed", self, "quit")
+
+func play():
+	get_tree().paused = false
+	visible = false
+
+func pause():
+    get_tree().paused = true
+    visible = true
+
+func quit():
+	get_tree().quit()
